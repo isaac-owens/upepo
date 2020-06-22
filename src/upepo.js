0eec0d5f5c13fc72441df99ef1ca95664ddb19ae
Isaac Owens <isaac.owens832@gmail.com>
2020-06-22 15:04:28 -0400

    Get camera permission to pop up in window without errors

--- src/upepo.js
+++ src/upepo.js
@@ -1,3 +1,25 @@
 // any javascript code imported here will be bundled into bundle.js by Webpack
 
+
 console.log('Hello from Webpack UPEPO!');
+
+// async function getMedia(constraints) {
+//   let stream = null;
+
+//   try {
+//     stream = await navigator.mediaDevices.getUserMedia(constraints);
+//   } catch(err) {
+//     console.log('Uh-oh! Looks like something went terribly wrong.');
+//     console.log(err);
+//   }
+// }
+
+var constraints = { audio: false, video: { facingMode: "user" } };
+navigator.mediaDevices.getUserMedia(constraints)
+.then(function(stream) {
+
+})
+.catch(function(err) {
+  console.log("Uh-oh! Looks like something went terribly wrong.");
+  console.log(err);
+})
