f38aa01bd72cdd88edd56a9fd0de18a3a935eea1
Isaac Owens <isaac.owens832@gmail.com>
2020-06-23 16:32:49 -0400

    Fix null element type error by fixing id attribute of canvas misspelling

--- src/upepo.js
+++ src/upepo.js
@@ -1,16 +1,14 @@
 // any javascript code imported here will be bundled into bundle.js by Webpack
 console.log('Hello from Webpack UPEPO!');
 
-// async function getMedia(constraints) {
-//   let stream = null;
+  let testAreaPos = [115, 240];
+  let timeOut, lastImageData;
 
-//   try {
-//     stream = await navigator.mediaDevices.getUserMedia(constraints);
-//   } catch(err) {
-//     console.log('Uh-oh! Looks like something went terribly wrong.');
-//     console.log(err);
-//   }
-// }
+  let testArea = document.getElementById('test-area');
+  
+  let canvasBlended = document.getElementById("canvas-blended");
+  let contextBlended = canvasBlended.getContext("2d");
+
 
 var constraints = { audio: false, video: { facingMode: "user" } };
 navigator.mediaDevices.getUserMedia(constraints)
@@ -26,19 +24,6 @@ navigator.mediaDevices.getUserMedia(constraints)
   console.log(err);
 })
 
-window.onLoad = () => {
-  let testAreaPos = [115, 240];
-  let timeOut, lastImageData;
-
-  let canvas = document.getElementById('canvas');
-  let context = canvas.getContext('2d');
-  
-  let canvasBlended = document.getElementById("canvas-blended");
-  let contextBlended = canvasBlended.getContext("2d");
-  
-  context.translate(canvas.width, 0);
-  context.scale(-1, 1);
-}
 
 /* 
 executed 60 times per second, calls functions to:
@@ -47,10 +32,24 @@ executed 60 times per second, calls functions to:
   detect the motion 
  */
 
+window.requestAnimFrame = (function () {
+  return (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+      window.setTimeout(callback, 1000 / 60);
+    }
+  );
+})();
+
 function update() {
   drawVideo();
   blend();
-  timeOut = setTimeout(update, 1000 / 60);
+  checkArea();
+  requestAnimationFrame(update);
 }
 
 function drawVideo() {
@@ -118,28 +117,5 @@ function blend() {
 }
 
 function checkArea() {
-  // loop over test area
-  for (let i = 0; i < 2; i++) {
-    // get pixes in area from blended image
-    let blendedData = contextBlended.getImageData(
-      test[i].area.x,
-      test[i].area.y,
-      test[i].area.width,
-      test[i].area.height,
-    )
-    let j = 0;
-    let average = 0;
-    // loop over pixels
-    while (j < (blendedData.data.length / 4)) {
-      // make an average between color channel
-      average += (blendedData.data[i * 4] + blendedData.data[i * 4 + 1] + blendedData.data[i * 4 + 2]) / 3;
-      j++;
-    }
-
-    // calculate average between color values of note area
-    average = Math.round(average / (blendedData.data.length / 4));
-    if (average > 10) {
-      console.log("Movement detected in test area!");
-    }
-  }
+  // loop over pixels in test area
 }
